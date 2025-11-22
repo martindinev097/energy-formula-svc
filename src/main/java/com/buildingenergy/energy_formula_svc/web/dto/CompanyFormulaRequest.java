a70a2df814557d772476de17fe9e187c626cc6de
Martin Dinev <martindinev10@gmail.com>
2025-11-22 00:25:35 +0000

    chore: add constructors to CompanyFormulaRequest for easier test setup

--- src/main/java/com/buildingenergy/energy_formula_svc/web/dto/CompanyFormulaRequest.java
+++ src/main/java/com/buildingenergy/energy_formula_svc/web/dto/CompanyFormulaRequest.java
@@ -1,10 +1,14 @@
 package com.buildingenergy.energy_formula_svc.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CompanyFormulaRequest {
 
     private BigDecimal pricePerKwh;
