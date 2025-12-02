b613589859676c5c8b8b8e3ab6888947044af20d
Martin Dinev <martindinev10@gmail.com>
2025-12-02 14:34:03 +0000

    feat: enable @EnableScheduling in EnergyFormulaSvcApplication

--- src/main/java/com/buildingenergy/energy_formula_svc/EnergyFormulaSvcApplication.java
+++ src/main/java/com/buildingenergy/energy_formula_svc/EnergyFormulaSvcApplication.java
@@ -2,8 +2,10 @@ package com.buildingenergy.energy_formula_svc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EnergyFormulaSvcApplication {
 
 	public static void main(String[] args) {
